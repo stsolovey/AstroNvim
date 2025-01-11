82cba83debbae4e80b953217159a4abda9a9b047
stsolovey <st.solovey@gmail.com>
2025-01-12 00:32:16 +0300

    refactor(none-ls): simplify golangci-lint configuration
    - Enable debug mode
    - Remove custom diagnostics format and configuration
    - Clean up commented code

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,50 +1,18 @@
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
+    opts.debug = true -- включаем отладку
     local null_ls = require "null-ls"
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
       -- Linter
-      null_ls.builtins.diagnostics.golangci_lint.with {
-        diagnostic_config = {
-          underline = true,
-          virtual_text = true,
-          signs = true,
-          update_in_insert = false,
-          severity_sort = true,
-        },
-      },
+      null_ls.builtins.diagnostics.golangci_lint,
+      -- .with {
+      --   -- Убираем #{s} из формата, чтобы не показывать W: / E: в тексте
+      --   diagnostics_format = "#{m}",
+      -- },
       -- Formatters
       null_ls.builtins.formatting.gofumpt,
       null_ls.builtins.formatting.goimports,
     })
   end,
 }
-
--- return {
---   "nvimtools/none-ls.nvim",
---   opts = function(_, opts)
---     local null_ls = require "null-ls"
---     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
---       -- Linter with optimizations
---       null_ls.builtins.diagnostics.golangci_lint.with({
---         diagnostics_format = "[#{c}] #{m} (#{s})",
---         args = function(params)
---           return {
---             "run",
---             "--out-format=json",
---             "--path-prefix", params.root,
---             "--config=" .. params.root .. "/.golangci.yml",  -- Explicitly point to config
---             params.filename
---           }
---         end,
---         debounce = 1000,
---         runtime_condition = function(params)
---           return params.event == "BufWritePost"
---         end,
---       }),
---       -- Formatters
---       null_ls.builtins.formatting.gofumpt,
---       null_ls.builtins.formatting.goimports,
---     })
---   end,
--- }
